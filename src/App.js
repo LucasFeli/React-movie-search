8a037ed13d1b6761fd85324ff27ed4398165d6c7
LucasFeli <felipe298@gmail.com>
2021-05-11 00:54:16 +0200

    consume the API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import SearchMovie from "./Components/Search/Search"
-import './App.css';
+import SearchMovie from "./Components/Search/Search";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1 className="title">React Movie Search</h1>
-      <SearchMovie/>
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovie />
       </div>
     </div>
   );
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function Search() {
-    return (
-        <div>
-            <form className="form">
-                <label className="label" htmlFor="query">Movie Name</label>
-                <input className="input" type="text" name="query" placeholder ="locking for.."></input>
-                <button className="button" type="submit">Search</button>
-            </form>
-        </div>
-    )
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    const query = "Harry Potter";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=9eae48419c789596b48dfdb7944f3050&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="locking for.."
+        ></input>
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
