8610ca094bb0d9ab9c65f010850f567b7a87e1dd
LucasFeli <felipe298@gmail.com>
2021-05-11 01:38:06 +0200

    using map

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Search() {
+
+  const [query, setQuery] = useState("")
+  const [movies, setMovies] = useState([])
+
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting");
-
-    const query = "Harry Potter";
+    
+   
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=9eae48419c789596b48dfdb7944f3050&language=en-US&query=${query}&page=1&include_adult=false`;
 
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        setMovies(data.results);
+    }catch(error){
+        console.error(error)
+    }
   };
 
   return (
@@ -25,11 +32,20 @@ export default function Search() {
           type="text"
           name="query"
           placeholder="locking for.."
+          value={query} onChange={(e)=>setQuery(e.target.value)}
         ></input>
         <button className="button" type="submit">
           Search
         </button>
       </form>
+        <div className="card-list" >
+            {movies.map(movie => (
+            <div className="card" key={movie.id}>
+                {movie.title}
+            </div>
+            ))}
+        </div>
+
     </div>
   );
 }
